3fffc3d16234050ee07cf120afa55c213c8825d3
rikidere <rikidere@public-docking-hg-6039.ethz.ch>
2018-02-22 11:05:54 +0100

    t

--- test/test.js
+++ test/test.js
@@ -1,4 +1,4 @@
 const api = require('../src/index');
-const Client = new api.Client('insert key here');
+const Client = new api.Client(`${"instert key here"}`);
 
 Client.beatmap.getById("1458418").then((bm) => console.log(`${bm.artist} - ${bm.title}`));
